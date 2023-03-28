8e368ffff4426691893cad4d1b3a3f7c60fa7d42
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-03-29 01:03:55 +0900

    読み取り、書き込み専用メソッド

--- chapter7/attr_accessor_reader_writer.rb
+++ chapter7/attr_accessor_reader_writer.rb
@@ -1,4 +1,22 @@
 class User
+  # @nameを読み書きするメソッドが自動的に定義される
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  # nameメソッドやname=メソッドを明示的に定義する必要がない
+end
+user = User.new('Alice')
+# @nameを変更する
+user.name = 'Bob'
+# @nameを参照する
+user.name #=> "Bob"
+
+# ----------------------------------------
+
+class User
   # 読み取り用のメソッドだけを定義する
   attr_reader :name
 
