d36afedbf62605b3e925f0b39cf5d357a1731216
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-03-29 12:06:47 +0900

    オーバーライドによる処理の上書き

---
+++ chapter7/overrride.rb
@@ -0,0 +1,31 @@
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def to_s
+    "name: #{name}, price: #{price}"
+  end
+end
+
+
+class DVD < Product
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    super(name, price)
+    @running_time = running_time
+  end
+
+  def to_s
+    # superでスーパークラスのto_sメソッドを呼び出す
+    "#{super}, running_time: #{running_time}"
+  end
+end
+
+
+dvd = DVD.new('An awesome film', 3000, 120)
+p dvd.to_s #=> "name: An awesome film, price: 3000, running_time: 120"
