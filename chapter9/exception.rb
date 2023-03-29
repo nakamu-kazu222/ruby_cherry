5eea5c2f952b9811179ddc153fe685a785e13acc
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-03-29 15:04:23 +0900

    例外処理の例文

--- chapter9/exception.rb
+++ chapter9/exception.rb
@@ -9,3 +9,37 @@ end
 
 # 例外処理を組み込んだので、最後まで実行可能
 puts 'End.'
+
+# ----------------------------------------
+
+# method_1にだけ例外処理を記述する
+def method_1
+  puts 'method_1 start.'
+  begin
+    method_2
+  rescue
+    puts '例外が発生しました'
+  end
+  puts 'method_1 end.'
+end
+
+def method_2
+  puts 'method_2 start.'
+  method_3
+  puts 'method_2 end.'
+end
+
+def method_3
+  puts 'method_3 start.'
+  # ZeroDivisionErrorを発生させる
+  1 / 0
+  puts 'method_3 end.'
+end
+
+# 処理を開始する
+method_1
+#=> method_1 start.
+#   method_2 start.
+#   method_3 start.
+#   例外が発生しました
+#   method_1 end.
