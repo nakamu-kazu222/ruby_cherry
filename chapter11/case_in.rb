bc3e725c6fe4a0edbae37932a2e55755d80d4932
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-03-30 17:57:34 +0900

    case_inによるパターンマッチ

--- chapter11/case_in.rb
+++ chapter11/case_in.rb
@@ -8,16 +8,13 @@ records = [
 require 'date'
 
 records.map do |record|
-  case record.size
-  when 1
-    # 年を指定、月と日は1固定
-    Date.new(record[0], 1, 1)
-  when 2
-    # 年月を指定、日は1固定
-    Date.new(record[0], record[1], 1)
-  when 3
-    # 年月日を指定
-    Date.new(record[0], record[1], r[2])
+  case record
+  in [y]
+    Date.new(y, 1, 1)
+  in [y, m]
+    Date.new(y, m, 1)
+  in [y, m, d]
+    Date.new(y, m, d)
   end
 end
 #=> [#<Date: 2021-01-01 ((2459216j,0s,0n),+0s,2299161j)>,
